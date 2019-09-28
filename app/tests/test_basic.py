40f2ffdec1dea1046cbd23a5c07605b2d8d079da
Syamantak Mukhopadhyay <mukherjee.syamantak@gmail.com>
2019-09-28 01:16:24 +0100

    add test to create task

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -1,10 +1,12 @@
 import os
 import unittest
+import json
 from app import app, db
 
 
 TEST_DB = 'test.db'
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class BasicTests(unittest.TestCase):
 
@@ -28,6 +30,16 @@ class BasicTests(unittest.TestCase):
     def tearDown(self):
         pass
 
+    ########################
+    #### helper methods ####
+    ########################
+
+    def add_task(self, title, description):
+        return self.app.post(
+            '/tasks',
+            data=json.dumps(dict(title=title, description=description)),
+            content_type='application/json'
+        )
 
     ###############
     #### tests ####
@@ -36,6 +48,10 @@ class BasicTests(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_add_task_is_successful(self):
+        response = self.add_task(title="test task", description="test task")
+        self.assertEqual(response.status_code, 201)
 
 
 if __name__ == "__main__":
